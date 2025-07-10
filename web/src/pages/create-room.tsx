4fe2546d4cebbc9482114878d776bb241221e6d0
Guilherme Felix <f3lixguilherme@gmail.com>
2025-07-10 14:39:00 -0300

    feat: pages create room, create question, room, question .... varias coisas

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,4 +1,5 @@
 import { RoomList } from "@/components/rooms-list";
+import { CreateRoomForm } from "@/components/create-room-form";
 
 export function CreateRoom() {
 
@@ -6,6 +7,7 @@ export function CreateRoom() {
     <div className="min-h-screen px-4 py-8">
       <div className="mx-auto max-w-4xl">
         <div className="grid grid-cols-2 items-start gap-8">
+          <CreateRoomForm />
           <RoomList />
         </div>
       </div>
